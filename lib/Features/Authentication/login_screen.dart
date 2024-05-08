b8b825b533da0be06c93ac846ff0f403ac8816dc
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-05-08 21:29:31 +0500

    login screen done

--- lib/Features/Authentication/login_screen.dart
+++ lib/Features/Authentication/login_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:test_task/Constant/app_colors.dart';
 import 'package:test_task/Features/Authentication/widgets/custom_button.dart';
 import 'package:test_task/Features/Authentication/widgets/custom_text_field.dart';
